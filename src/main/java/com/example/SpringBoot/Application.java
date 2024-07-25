49ec13c7c442e041688215ffe377ccc4c039cab7
Satyavan-Khandekar <satyavankhandekar3@gmail.com>
2024-07-25 14:07:19 +0530

    update main class

--- src/main/java/com/example/SpringBoot/Application.java
+++ src/main/java/com/example/SpringBoot/Application.java
@@ -1,15 +1,24 @@
 package com.example.SpringBoot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
 
+	public static final Logger logger = LoggerFactory.getLogger(Application.class);
 	public static void main(String[] args) {
 
+		logger.info("It's a continuous integration job");
 		SpringApplication.run(Application.class, args);
-		System.out.println("This is continuous integration example");
+
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("It's second integration job");
+	}
 }
